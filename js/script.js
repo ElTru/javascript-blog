32a03b2076be83c5a40a7e893c1fdc91e00d1963
ElTru <elzbieta.trusiewicz@gmail.com>
2020-11-18 20:12:28 +0100

    Add class active for clickedElement

--- js/script.js
+++ js/script.js
@@ -1,37 +1,29 @@
-'use strict'
-
-/* document.getElementById('test-button').addEventListener('click', function(){
-  const links = document.querySelectorAll('.titles a');
-  console.log('links:', links);
-}); */
-
-const titleClickHandler = function(){
+const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
-}
-  /* remove class 'active' from all article links  */
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+  /* [DONE] remove class 'active' from all article links  */
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active'); //.classList biblioteka z info o tych klasach
-}
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* add class 'active' to the clicked link */
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
-const activeArticles = document.querySelectorAll('.posts article.active');
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active'); 
-}
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
+}
 
 const links = document.querySelectorAll('.titles a');
 
