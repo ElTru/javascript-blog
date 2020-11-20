74b993a4fd6df733c34303475ad182fa2e6ae9ed
ElTru <elzbieta.trusiewicz@gmail.com>
2020-11-20 15:48:07 +0100

    Add generateTilteLinks

--- js/script.js
+++ js/script.js
@@ -37,8 +37,30 @@ const titleClickHandler = function(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post', // zawartośc artykułu
+  optTitleSelector = '.post-title', // tytuły artykułów w poście
+  optTitleListSelector = '.titles'; // tytuły artykułów jako linki w linście
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(){
+  /* [DONE] remove contents of titleList */ // wywalić lini z lewej kolumny
+  const titleList = document.querySelector(optTitleListSelector); // znajduje linki i wrzucam pustaka ''
+  titleList.innerHTML = '';
+
+  /* [IN PROGRESS] for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for(let article of articles){
+    const articleId = article.getAttribute('id'); // get the article id
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;   // find the title element
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>'; //get the title from the title element // create HTML of the link
+    html = html + linkHTML;  // insert link html variable
+  }
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+
+generateTitleLinks();
